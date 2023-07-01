c9cc782b7be1bc987b1edffbd27715ae88557f01
Vika14 <manunya1406@gmail.com>
2023-07-01 17:03:14 +0200

    hw8-8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+function onTimeupdate(e) {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+    console.log(e.seconds);
+}
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+if (currentTime) {
+    player.setCurrentTime(currentTime);
+}
